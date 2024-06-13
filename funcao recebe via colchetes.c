80283999b712cbaec5bcd3d6b5cd469700ab0ee0
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-06-13 10:49:39 -0300

    Create funcao recebe via colchetes.c

---
+++ funcao recebe via colchetes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprima(int vetor[], int n){
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("Vetor[%d]: %d\n",i , vetor[i]);
+    }
+}
+
+int main(){
+    int vetor[] = {1,2,3,4,5};
+    int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+    imprima(vetor, tamanho);
+
+    return 0;
+}
